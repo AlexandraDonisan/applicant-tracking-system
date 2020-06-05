c3b8dd86504bb08379b836593019817142f0213e
Sandy <alexandra_donisan@yahoo.com>
2020-06-05 08:58:46 +0300

    See matching and missing skills + Handle of job Keywords - Checked

--- candidates/api/urls.py
+++ candidates/api/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from rest_framework import routers
-from .views import CandidateViewSet, KeywordsViewSet, JobViewSet, \
-    compute_score_view, get_similar_cvs_view, summarize_all_cvs
+from .views import CandidateViewSet,  \
+    compute_score_view, get_similar_cvs_view, summarize_all_cvs, get_matching_skills, get_missing_skills
 
 router = routers.DefaultRouter()
 router.register('candidates', CandidateViewSet, 'candidates')
-router.register('keywords', KeywordsViewSet, 'keywords')
-router.register('job', JobViewSet, 'job')
 
 # router.register('<The URL prefix>', <The view set class>, '<The URL name>')
 # We use three arguments to the register() method, but the third argument is not required.
@@ -15,6 +13,8 @@ urlpatterns = [
     path('candidate/compute_score/', compute_score_view, name='compute_score'),
     path('candidate/get_similar_cvs/', get_similar_cvs_view, name='compute_score'),
     path('candidate/summarize_cvs/', summarize_all_cvs, name='summarize_all_cvs'),
+    path('candidate/matching_skills/<int:id>/', get_matching_skills, name='get_matching_skills'),
+    path('candidate/missing_skills/<int:id>/', get_missing_skills, name='get_missing_skills'),
 ]
 
 urlpatterns += router.urls
